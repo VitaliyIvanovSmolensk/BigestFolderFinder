4e6892db1552c653c59ff7040f511fb54c37cb9b
vitalii_ivanov <vip-iq@yandex.ru>
2022-09-27 21:24:03 +0300

    Шаг 2.
    Метод определения размера папки

---
+++ src/FolderSizeCalculator.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+public class FolderSizeCalculator extends RecursiveTask <Long> {
+    private File folder;
+
+    public FolderSizeCalculator(File folder) {
+        this.folder = folder;
+    }
+
+    @Override
+    protected Long compute() {
+        if (folder.isFile()){
+            return folder.length();
+        }
+         long sum = 0;
+        File [] files = folder.listFiles();
+        List<FolderSizeCalculator> subTask = new LinkedList<>();
+        for (File file : files){
+            FolderSizeCalculator task = new FolderSizeCalculator(file);
+            task.fork();
+            subTask.add(task);
+        }
+        for (FolderSizeCalculator task :subTask){
+            sum += task.join();
+        }
+        return sum;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "d:/Games/";
         File file = new File(folderPath);
         long start = System.currentTimeMillis();
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(getHumanRedableSize(size));
+
+
+//        System.out.println(getFolderSize(file));
+
         System.out.println(System.currentTimeMillis() - start);
 
 
@@ -21,5 +29,41 @@ public class Main {
             sum += getFolderSize(file);
         }
         return  sum;
+    }
+    public static String getHumanRedableSize(long size){
+                if (size / 1024 < 1 ){
+            return size + "B";
+        } else if (size / 1024 < 1024) {
+            return Math.round(size/1024L) + "Kb";
+        }else if (size / (1024*1024) < 1024) {
+            return Math.round(size/(1024L*1024)) + "Mb";
+        }else if (size / (1024*1024*1024) < 1024) {
+            return Math.round(size/(1024L*1024*1024)) + "Gb";
+        }else {
+            return Math.round(size/(1024L*1024*1024*1024)) + "Tb";
+        }
+    }
+    public static Long getSizeFromeHumanReadble (String size){
+        long volium = 0l;
+        for (int i = 0; i < size.length();i++){
+            char lit = size.charAt(i);
+            if (lit == 'B') {
+                volium = Long.parseLong(size.substring(0, i));
+                return volium;
+            } else if (lit == 'K') {
+                volium = Long.parseLong(size.substring(0, i));
+                return volium * 1024;
+            } else if (lit == 'M') {
+                volium = Long.parseLong(size.substring(0, i));
+                return volium * 1024*1024;
+            } else if (lit == 'G') {
+                volium = Long.parseLong(size.substring(0, i));
+                return volium * 1024*1024*1024;
+            } else if (lit == 'T') {
+                volium = Long.parseLong(size.substring(0, i));
+                return volium * 1024*1024*1024*1024;
+            }
+        }
+        return 0l;
     }
 }
